893bbf28cf88cecd0c459810fbcfb4fbbedf1e33
Juan C. Müller <juan@challengepost.com>
2011-08-13 12:04:37 -0400

    Add some logging.

--- GDataYoutubeUploadTest/GDataYoutubeUploadTest/GDataYoutubeUploadTestViewController.m
+++ GDataYoutubeUploadTest/GDataYoutubeUploadTest/GDataYoutubeUploadTestViewController.m
@@ -40,6 +40,9 @@
     
     NSLog(@"Auth object? %d Can Authorize? %d", auth, [auth canAuthorize]);
     
+    if ([auth canAuthorize]){
+        NSLog(@"Authenticated as: %@, verified: %@", [auth userEmail], [auth userEmailIsVerified]);
+    }
     [[self youTubeService] setAuthorizer:auth];
     
     [super viewDidLoad];
@@ -91,7 +94,7 @@
     if (error != nil) {
         // Authentication failed
         NSLog(@"failed, auth: %@", auth);
-        NSLog(@"failed, can authenticate? %d, error: %@", [auth canAuthorize], error);
+        NSLog(@"failed, can authenticate? %d, auth: %@", [auth canAuthorize], auth);
 //        [textView setText:[NSString stringWithFormat:@"failed, can authenticate? %d, error: %@",
 //                           [authentication canAuthorize],
 //                           error]];
