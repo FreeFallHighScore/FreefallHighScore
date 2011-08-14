269b2328a4e3a326724ea9203ca12685c4f2ad57
Juan C. Müller <juan@challengepost.com>
2011-08-14 15:11:57 -0400

    Experimenting...

---
+++ TestingObjectiveResource/TestingObjectiveResourceTests/TestingObjectiveResourceTests.m
@@ -0,0 +1,32 @@
+//
+//  TestingObjectiveResourceTests.m
+//  TestingObjectiveResourceTests
+//
+//  Created by Juan C. Müller on 8/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestingObjectiveResourceTests.h"
+
+@implementation TestingObjectiveResourceTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestingObjectiveResourceTests");
+}
+
+@end
