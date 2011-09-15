203f4abff587a049a06601db2b3d8c73e5261da2
obviousjim <james@jamesgeorge.org>
2011-09-15 12:35:31 -0400

    final commit
    Signed-off-by: obviousjim <james@jamesgeorge.org>

--- FFHS/JSON/JSON.h
+++ FFHS/JSON/JSON.h
@@ -4,7 +4,7 @@
 //
 //  Created by Stig Brautaset on 01/06/2011.
 //  Copyright 2011 Stig Brautaset. All rights reserved.
-//
+// Hunter and Johnson
 
 #warning The JSON.h header is deprecated, and will disappear in a future release. Please change to include SBJson.h instead.
 #include "SBJson.h"
